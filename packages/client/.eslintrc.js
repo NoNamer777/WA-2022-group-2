7abab9c6bf9b1d05976314b36f587c12e9c8845b
NoNamer777 <oscar.wellner@gmail.com>
2023-04-24 21:21:29 +0200

    Adjust Prettier and linting rules

---
+++ packages/client/.eslintrc.js
@@ -0,0 +1,12 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: false,
+  extends: ['plugin:vue/vue3-essential', '../../.eslintrc.js'],
+  rules: {
+    'vue/require-default-prop': 'off',
+    'vue/html-indent': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase']
+  }
+}
