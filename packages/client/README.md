32483306d40dff7d29135cbaa56fc7278eb813b0
NoNamer777 <oscar.wellner@gmail.com>
2023-04-24 19:05:03 +0200

    Move client code into its own folder

--- packages/client/README.md
+++ packages/client/README.md
@@ -1,4 +1,4 @@
-# wa-2023
+# WA-2023-group-2: Client
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ packages/client/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import NavigationBar from './components/NavigationBar.vue'
+</script>
+
+<template>
+  <div class="container-sm h-100 d-flex flex-column">
+    <NavigationBar />
+
+    <RouterView class="box bg-white mt-5" />
+  </div>
+</template>
+
+<style scoped>
+.box {
+  padding: 3rem;
+  flex: 1 1 auto;
+  opacity: 0.9;
+}
+
+@media screen and (min-width: 580px) {
+  .box {
+    padding: 6rem;
+  }
+}
+</style>
