40840dcb2790ee5b95f3ad6c3e1c4e1d1f2e1337
NoNamer777 <oscar.wellner@gmail.com>
2023-04-24 19:07:43 +0200

    Adjust paths after client code move

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -5,10 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // TODO: Provide via node environment variable
+  base: '/',
+  // TODO: Provide via node environment variable
+  mode: 'development',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('packages/client/src', import.meta.url))
     }
-  }
+  },
+  // TODO: Provide via node environment variable
+  root: 'packages/client'
 })
