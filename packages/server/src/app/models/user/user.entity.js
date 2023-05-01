0422d38237add587f1f4dda368d94f9b363233a2
NoNamer777 <oscar.wellner@gmail.com>
2023-05-01 13:27:12 +0200

    Add UserEntity

---
+++ packages/server/src/app/models/user/user.entity.js
@@ -0,0 +1,29 @@
+const { DataTypes, Model } = require('sequelize')
+
+class UserEntity extends Model {}
+
+const UserModelDefinition = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: 'unique_user_username_idx'
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}
+
+module.exports = { UserEntity, UserModelDefinition }
