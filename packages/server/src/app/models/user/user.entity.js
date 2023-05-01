f3a996e1e7f87e7179fa36a8a1e31ab9956533f7
NoNamer777 <oscar.wellner@gmail.com>
2023-05-01 19:41:19 +0200

    Add validation rules

--- packages/server/src/app/models/user/user.entity.js
+++ packages/server/src/app/models/user/user.entity.js
@@ -6,24 +6,44 @@ class UserEntity extends Model {}
 const UserModelDefinition = {
   id: {
     type: DataTypes.INTEGER,
+    allowNull: false,
     primaryKey: true,
-    autoIncrement: true
+    autoIncrement: true,
+    validate: {
+      notNull: true,
+      isInt: true,
+      min: 0
+    }
   },
   username: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: 'unique_user_username_idx'
+    unique: 'unique_user_username_idx',
+    validate: {
+      notNull: true,
+      notEmpty: true,
+      len: [3, 80]
+    }
   },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
     validate: {
-      isEmail: true
+      notNull: true,
+      notEmpty: true,
+      isEmail: true,
+      len: [3, 80]
     }
   },
   password: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      notNull: true,
+      notEmpty: true,
+      len: [3, 124],
+      is: /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_\-+=;:<>.?()])[a-zA-Z0-9!@#$%^&*_\-+=;:<>.?()]+/g
+    }
   }
 }
 
