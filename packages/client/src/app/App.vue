0258c11c1bf68c0a0ac1f963c209f50b32403657
Marjo Salo <75611648+majsa1@users.noreply.github.com>
2023-05-02 11:57:52 +0200

    Adjust opacity to affect background box only

--- packages/client/src/app/App.vue
+++ packages/client/src/app/App.vue
@@ -7,7 +7,7 @@ import { NavigationBar } from './components'
   <div class="container h-100 d-flex flex-column">
     <NavigationBar />
 
-    <RouterView class="box bg-white mt-5" />
+    <RouterView class="box mt-5" />
   </div>
 </template>
 
@@ -15,7 +15,7 @@ import { NavigationBar } from './components'
 .box {
   padding: 2rem;
   flex: 1 1 auto;
-  opacity: 0.9;
+  background: rgba(255, 255, 255, 0.9);
 }
 
 @media screen and (min-width: 580px) {
