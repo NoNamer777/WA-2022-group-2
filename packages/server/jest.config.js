b71e15fe3e6a9c1546fc8719868facc21c9d2520
Oscar Wellner <oscar.wellner@gmail.com>
2023-05-02 11:03:23 +0200

    Add jest configurations

---
+++ packages/server/jest.config.js
@@ -0,0 +1,35 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ * @type {import('jest').Config}
+ */
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/app/**/*.js',
+    '!<rootDir>/src/app/**/*.controller.js',
+    '!<rootDir>/src/app/**/*.router.js',
+    '!<rootDir>/src/app/app.js'
+  ],
+  coverageDirectory: '../../coverage/server',
+  coveragePathIgnorePatterns: ['/node_modules/', '/client/'],
+  coverageProvider: 'v8',
+  coverageReporters: ['text-summary', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  displayName: 'Wasted: Server',
+  reporters: undefined,
+  randomize: true,
+  roots: ['<rootDir>'],
+  setupFiles: [],
+  setupFilesAfterEnv: [],
+  testMatch: ['<rootDir>/src/app/**/*.test.js'],
+  testPathIgnorePatterns: ['/node_modules/', '/client/']
+}
