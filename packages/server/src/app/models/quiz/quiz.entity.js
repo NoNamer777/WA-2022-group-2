0ab8a10bbcf061f10e19cca34f0f4e8a79a1fb12
Ahmed Benhajar <ahmedbenhajar@hotmail.com>
2023-05-10 19:00:15 +0200

    Added quiz entity & migration

---
+++ packages/server/src/app/models/quiz/quiz.entity.js
@@ -0,0 +1,27 @@
+const { DataTypes, Model } = require('sequelize')
+const DatabaseService = require('../../services/database.service')
+
+class QuizEntity extends Model {}
+
+/** @type {import('sequelize').ModelAttributes<QuizEntity>} */
+const QuizModelDefinition = {
+  id: {
+    type: DataTypes.INTEGER(11),
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING(255)
+  }
+}
+
+QuizEntity.init(QuizModelDefinition, {
+  sequelize: DatabaseService.instance().sequelizeInstance,
+  modelName: 'quiz',
+  tableName: 'quiz',
+  createdAt: false,
+  updatedAt: false
+})
+
+module.exports = { QuizEntity, QuizModelDefinition }
