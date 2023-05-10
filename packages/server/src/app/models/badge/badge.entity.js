2fb3b1be8bbb1a4128a86251b6556ecabd44e1eb
Ahmed Benhajar <ahmedbenhajar@hotmail.com>
2023-05-10 18:58:42 +0200

    Added badge entity & migration

---
+++ packages/server/src/app/models/badge/badge.entity.js
@@ -0,0 +1,30 @@
+const { DataTypes, Model } = require('sequelize')
+const DatabaseService = require('../../services/database.service')
+
+class BadgeEntity extends Model {}
+
+/** @type {import('sequelize').ModelAttributes<BadgeEntity>} */
+const BadgeModelDefinition = {
+  id: {
+    type: DataTypes.INTEGER(11),
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING(255)
+  },
+  image_path: {
+    type: DataTypes.STRING(255)
+  }
+}
+
+BadgeEntity.init(BadgeModelDefinition, {
+  sequelize: DatabaseService.instance().sequelizeInstance,
+  modelName: 'badge',
+  tableName: 'badge',
+  createdAt: false,
+  updatedAt: false
+})
+
+module.exports = { BadgeEntity, BadgeModelDefinition }
