2c6af5d87c34f3c6cb7993ef031563914d788764
Ahmed Benhajar <ahmedbenhajar@hotmail.com>
2023-05-10 19:01:22 +0200

    Added user_quiz entity & migration

---
+++ packages/server/src/app/models/user_quiz/user_quiz.entity.js
@@ -0,0 +1,50 @@
+const { DataTypes, Model } = require('sequelize')
+const DatabaseService = require('../../services/database.service')
+
+class UserQuizEntity extends Model {}
+
+/** @type {import('sequelize').ModelAttributes<UserQuizEntity>} */
+const UserQuizModelDefinition = {
+  id: {
+    type: DataTypes.INTEGER(11),
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  user_id: {
+    type: DataTypes.INTEGER(11),
+    references: {
+      model: 'user',
+      key: 'id'
+    },
+    onDelete: 'CASCADE',
+    allowNull: false
+  },
+  quiz_id: {
+    type: DataTypes.INTEGER(11),
+    references: {
+      model: 'quiz',
+      key: 'id'
+    },
+    onDelete: 'CASCADE',
+    allowNull: false
+  },
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false
+  },
+  points: {
+    type: DataTypes.INTEGER(11),
+    allowNull: false
+  }
+}
+
+UserQuizEntity.init(UserQuizModelDefinition, {
+  sequelize: DatabaseService.instance().sequelizeInstance,
+  modelName: 'user_quiz',
+  tableName: 'user_quiz',
+  createdAt: false,
+  updatedAt: false
+})
+
+module.exports = { UserQuizEntity, UserQuizModelDefinition }
