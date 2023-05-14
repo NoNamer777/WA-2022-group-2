73c56b2c5559b118a2c2d25976e81228bdee00e2
Oscar Wellner <oscar.wellner@gmail.com>
2023-05-14 19:27:46 +0200

    Add jsDoc in AuthenticationService

--- packages/client/src/app/services/auth.service.js
+++ packages/client/src/app/services/auth.service.js
@@ -1,6 +1,7 @@
 import { HttpRequestService } from './http-request.service'
 
 export class AuthService {
+  /** @return {AuthService} */
   static instance() {
     if (AuthService.#instance) return AuthService.#instance
 
@@ -8,12 +9,22 @@ export class AuthService {
     return AuthService.#instance
   }
 
+  /** @type {AuthService} */
   static #instance
 
+  /**
+   * @param username {string}
+   * @param password {string}
+   * @return {Promise<User>}
+   */
   async login(username, password) {
     return await HttpRequestService.instance().postRequest('/auth/login', { username, password })
   }
 
+  /**
+   * @param userData {Omit<User, 'id' | 'isAdmin'> & { passwordConfirm: string }}
+   * @return {Promise<User>}
+   */
   async register(userData) {
     if (userData.password !== userData.passwordConfirm) {
       throw new Error('Het bevestigde wachtwoord komt niet overeen met je opgegeven wachtwoord')
@@ -21,6 +32,9 @@ export class AuthService {
     return await HttpRequestService.instance().postRequest('/auth/register', userData)
   }
 
+  /**
+   * @return {Promise<void>}
+   */
   async logout() {
     return await HttpRequestService.instance().postRequest('/auth/logout', {})
   }
