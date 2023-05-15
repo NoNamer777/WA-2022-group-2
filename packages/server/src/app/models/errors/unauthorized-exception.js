08d0926a25474bbec637500c23263ab80cea7396
NoNamer777 <oscar.wellner@gmail.com>
2023-05-15 15:03:19 +0200

    Allow overriding of unauthorized exception message

--- packages/server/src/app/models/errors/unauthorized-exception.js
+++ packages/server/src/app/models/errors/unauthorized-exception.js
@@ -1,10 +1,10 @@
 const HttpException = require('./http.exception')
 
 class UnauthorizedException extends HttpException {
-  constructor() {
+  constructor(message) {
     // Pass 'Unauthorized' as message as well so that we don't give any indication of what is going
     // wrong in the authorization/authentication flow
-    super(401, 'Unauthorized', 'Unauthorized')
+    super(401, 'Unauthorized', message || 'Unauthorized')
   }
 }
 
