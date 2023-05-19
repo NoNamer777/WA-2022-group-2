27bb44cd8bba52c06a013c3cc99eb008e4000d89
Oscar Wellner <oscar.wellner@gmail.com>
2023-05-19 16:57:24 +0200

    Refactor jwtAuthHeaderValidator
    To handle an error only from the catch block

--- packages/server/src/app/middleware/jwt-auth-header-validator.js
+++ packages/server/src/app/middleware/jwt-auth-header-validator.js
@@ -5,18 +5,17 @@ import { JwtService } from '../services/jwt.service.js';
 export async function jwtAuthHeaderValidator(request, response, next) {
   let token = request.headers.authorization || null;
 
-  if (!token) {
-    // Header is not set on the request
-    next(new UnauthorizedException());
-  }
-  token = token.replace('Bearer ', '').trim();
-
-  if (token.length === 0) {
-    // No JWT token is provided after the Bearer
-    next(new UnauthorizedException());
-  }
-
   try {
+    if (!token) {
+      // Header is not set on the request
+      throw new UnauthorizedException();
+    }
+    token = token.replace('Bearer ', '').trim();
+
+    if (token.length === 0) {
+      // No JWT token is provided after the Bearer
+      throw new UnauthorizedException();
+    }
     const decodedToken = JwtService.instance().decodeToken(token);
 
     // Validate if the User is valid by retrieving the User's data by ID.
