c2995e21b758924dbff620b6f35c99450f932ccd
Oscar Wellner <oscar.wellner@gmail.com>
2023-05-19 11:22:25 +0200

    Adjust main to use await/async

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,53 +1,39 @@
 #!/usr/bin/env node
 
-const app = require('./app/app');
 const http = require('http');
+const app = require('./app/app');
+const ConfigService = require('./app/services/config.service');
 
-// Get port and hostname from environment and store in Express
-const host = process.env.SERVER_HOST || 'localhost';
-const port = normalizePort(process.env.SERVER_PORT || '8080');
-app.set('port', port);
-app.set('hostname', host);
+(async () => {
+  const wastedApp = await app();
 
-const server = http.createServer(app);
+  // Get port and hostname from the config service and store in Express
+  const host = ConfigService.instance().config.server.host;
+  const port = ConfigService.instance().config.server.port;
 
-server.listen(port, host);
-server.on('error', onError);
-server.on('listening', onListening);
+  wastedApp.app.set('port', port);
+  wastedApp.app.set('hostname', host);
 
-/**
- * Normalize a port into a number, string, or false.
- */
-function normalizePort(val) {
-  const port = parseInt(val, 10);
+  const server = http.createServer(wastedApp.app);
 
-  // named pipe
-  if (isNaN(port)) return val;
+  server.listen(port, host);
+  server.on('listening', () => console.info(`Server is Listening on http://${host}:${port}/`));
+  server.on('error', (error) => {
+    if (error.syscall !== 'listen') throw error;
 
-  // port number
-  if (port >= 0) return port;
-  return false;
-}
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-function onError(error) {
-  if (error.syscall !== 'listen') throw error;
-
-  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
-
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-function onListening() {
-  console.info(`Server is Listening on http://${host}:${port}/`);
-}
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  });
+})();
