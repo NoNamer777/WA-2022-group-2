4eacfb1d778d7cbacefcbd702d253d135671a525
Jennifer Goudswaard <Jennifer_Goudswaard-DG@hotmail.com>
2023-05-21 20:46:54 +0200

    Add: InfoView to index.js

--- packages/client/src/app/views/index.js
+++ packages/client/src/app/views/index.js
@@ -1,7 +1,8 @@
 import ChallengeActiveView from './ChallengeActiveView.vue';
 import ChallengeCreationView from './ChallengeCreationView.vue';
 import HomeView from './HomeView.vue';
+import InfoView from './InfoView.vue';
 import LoginView from './authentication/LoginView.vue';
 import RegisterView from './authentication/RegisterView.vue';
 
-export { ChallengeActiveView, ChallengeCreationView, HomeView, LoginView, RegisterView };
+export { ChallengeActiveView, ChallengeCreationView, HomeView, LoginView, RegisterView, InfoView };
