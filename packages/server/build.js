4f239eb6f6653ee80177402242d82c21ac6193c0
Oscar Wellner <oscar.wellner@gmail.com>
2023-05-21 18:49:54 +0200

    Minify build result and generate sourcemaps depending on environment

--- packages/server/build.js
+++ packages/server/build.js
@@ -7,11 +7,12 @@ import { build } from 'esbuild';
     color: true,
     external: ['pg-hstore'],
     format: 'cjs',
+    minify: true,
     outdir: 'dist/server',
     platform: 'node',
     publicPath: '/',
     target: 'ES2022',
     treeShaking: true,
-    sourcemap: true,
+    sourcemap: process.env.NODE_ENV !== 'production',
     write: true
   }))();
