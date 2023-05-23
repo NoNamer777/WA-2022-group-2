afac32ef2162c76b6c51ff410f2b57071a92ce50
Oscar Wellner <oscar.wellner@gmail.com>
2023-05-23 16:54:24 +0200

    Split configurations for building and serving

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -2,26 +2,41 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.VITE_CLIENT_BASE_URL || '/',
+/** @type {import('vite').UserConfig} */
+const sharedConfig = {
+  base: '/',
   build: {
     emptyOutDir: true,
-    outDir: '../../../dist/client',
-    manifest: true,
-    sourcemap: process.env.NODE_ENV !== 'production'
+    outDir: '../../../dist/client'
   },
   cacheDir: '../../../.vite',
-  css: {
-    devSourcemap: true
-  },
   envDir: fileURLToPath(new URL('../../environment', import.meta.url)),
-  mode: process.env.NODE_ENV || 'development',
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@wasted/client': fileURLToPath(new URL('packages/client/src', import.meta.url))
-    }
-  },
   root: 'packages/client/src'
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  if (mode === 'production') {
+    return {
+      ...sharedConfig,
+      build: {
+        ...sharedConfig.build,
+        manifest: true,
+        sourcemap: false
+      },
+      mode: mode
+    };
+  }
+  return {
+    ...sharedConfig,
+    build: {
+      ...sharedConfig.build,
+      sourcemap: true
+    },
+    css: {
+      devSourcemap: true
+    },
+    mode: mode
+  };
 });
