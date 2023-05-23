f68c8a658d667d90825c124ea7d65e2786e3ee59
NoNamer777 <oscar.wellner@gmail.com>
2023-05-23 08:30:08 +0200

    Allow DELETE requests and remove allowed HEAD requests

--- packages/server/src/app/core/middleware/cors-middleware.js
+++ packages/server/src/app/core/middleware/cors-middleware.js
@@ -4,7 +4,7 @@ import { ConfigService } from '../services/index.js';
 export function corsMiddleware() {
   const corsOptions = {
     origin: ConfigService.instance().config.server.allowedOrigins,
-    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Content-Type', 'Authorization']
   };
