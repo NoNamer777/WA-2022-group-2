18d677d93f83c182f11ff1ee79d7bbb8d7995561
NoNamer777 <oscar.wellner@gmail.com>
2023-05-23 08:30:28 +0200

    Provide fallback value when no allowed origins are passed

--- packages/server/src/app/core/middleware/cors-middleware.js
+++ packages/server/src/app/core/middleware/cors-middleware.js
@@ -3,7 +3,7 @@ import { ConfigService } from '../services/index.js';
 
 export function corsMiddleware() {
   const corsOptions = {
-    origin: ConfigService.instance().config.server.allowedOrigins,
+    origin: ConfigService.instance().config.server.allowedOrigins || [],
     methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Content-Type', 'Authorization']
