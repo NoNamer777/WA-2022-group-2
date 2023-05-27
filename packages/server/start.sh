bea745ca072c6ef00abc4d7584c26fb1cc49c6ff
Oscar Wellner <oscar.wellner@gmail.com>
2023-05-27 13:17:12 +0200

    Fix migrations in Docker containers
    To include the shared table definitions

--- packages/server/start.sh
+++ packages/server/start.sh
@@ -1,5 +1,5 @@
 # Run database migrations to make sure all the required tables and data are present in the database
-npm run server:run-migrations -- --env production --config ./database.json --migrations-path ./migrations
+npm run server:run-migrations -- --env production --config ./database.json --migrations-path ./db/migrations
 
 # Start the application
 node main.js
