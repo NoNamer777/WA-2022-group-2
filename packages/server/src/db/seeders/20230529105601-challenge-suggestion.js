278c01c1421295824cb40e6640c20cd9c1b729ca
Marjo Salo <75611648+majsa1@users.noreply.github.com>
2023-05-29 14:08:13 +0200

    Add challenge suggestion table

---
+++ packages/server/src/db/seeders/20230529105601-challenge-suggestion.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    return queryInterface.bulkInsert('challenge_suggestion', [
+      {
+        name: 'Herbruikbare zakjes en tassen meenemen naar de supermarkt'
+      },
+      {
+        name: 'Eigen drinkfles meenemen'
+      },
+      {
+        name: 'Fruit naar school in plaats van voorverpackte snacks'
+      },
+      {
+        name: 'Plasticvrij snacken onderweg'
+      },
+      {
+        name: 'Eigen mok gebruiken in plaats van wegwerpkoffiebekers'
+      },
+      {
+        name: 'Eigen brooddoos mee naar werk'
+      },
+      {
+        name: 'Fruit en groente zonder plastic kopen'
+      },
+      {
+        name: 'Shampoo -of zeepblok gebruiken in de badkamer'
+      }
+    ]);
+  },
+
+  async down(queryInterface) {
+    return queryInterface.bulkDelete('challenge_suggestion', null, {});
+  }
+};
