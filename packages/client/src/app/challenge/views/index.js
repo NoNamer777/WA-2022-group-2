9f251c846319dae41066bc5fe40631767554fb3e
Oscar Wellner <oscar.wellner@gmail.com>
2023-06-02 17:39:50 +0200

    Run Prettier

--- packages/client/src/app/challenge/views/index.js
+++ packages/client/src/app/challenge/views/index.js
@@ -2,4 +2,4 @@ import ChallengeCreationView from './ChallengeCreationView.vue';
 import ChallengeProgressView from './ChallengeProgressView.vue';
 import ChallengeView from './ChallengeView.vue';
 
-export { ChallengeProgressView, ChallengeCreationView, ChallengeView };
+export { ChallengeCreationView, ChallengeProgressView, ChallengeView };
