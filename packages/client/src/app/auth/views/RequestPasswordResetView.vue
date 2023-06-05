e53c4c9a447e6ae49adfd631e0dd10b04d93922d
Oscar Wellner <oscar.wellner@gmail.com>
2023-06-05 16:37:19 +0200

    Add success message

--- packages/client/src/app/auth/views/RequestPasswordResetView.vue
+++ packages/client/src/app/auth/views/RequestPasswordResetView.vue
@@ -1,29 +1,40 @@
 <template>
   <main class="container d-flex justify-content-center">
-    <section class="col-12 col-sm-10 col-lg-7" :class="{ 'd-none': done }">
-      <h1>Aanvraag Wachtwoord herstellen</h1>
-      <p>Welke gebruikersnaam gebruik je tijdens het inloggen?</p>
-      <FormKit
-        type="form"
-        :actions="false"
-        @submit="sendResetPasswordRequest"
-        #default="{ state: { valid } }"
-      >
-        <CustomFormKit
-          name="username"
-          label="Gebruikersnaam"
-          placeholder="johndoe"
-          validation="required"
-          v-model:model-value="username"
-        />
-        <CustomFormKit
-          type="submit"
-          label="Aanvraag indienen"
-          name="submit"
-          input-class="form-btn-primary"
-          :disabled="!valid"
-        ></CustomFormKit>
-      </FormKit>
+    <section class="col-12 col-sm-10 col-lg-7">
+      <template v-if="!done">
+        <h1>Aanvraag Wachtwoord herstellen</h1>
+        <p>Welke gebruikersnaam gebruik je tijdens het inloggen?</p>
+        <FormKit
+          type="form"
+          :actions="false"
+          @submit="sendResetPasswordRequest"
+          #default="{ state: { valid } }"
+        >
+          <CustomFormKit
+            name="username"
+            label="Gebruikersnaam"
+            placeholder="johndoe"
+            validation="required"
+            v-model:model-value="username"
+          />
+          <CustomFormKit
+            type="submit"
+            label="Aanvraag indienen"
+            name="submit"
+            input-class="form-btn-primary"
+            :disabled="!valid"
+          ></CustomFormKit>
+        </FormKit>
+      </template>
+      <template v-else>
+        <h1>Success</h1>
+        <p>
+          Er is zojuist een email verstuurd naar het email addres wat gekoppeld is aan je account.
+          Deze kan bij je binnen komen tussen nu en 5 minuten.
+          <br /><br />
+          Houd je inbox in de gaten om jou wachtwoord te veranderen.
+        </p>
+      </template>
     </section>
   </main>
 </template>
