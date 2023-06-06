f2aad4162d7c76291faafda281d20c571aaaa4c2
Ahmed Benhajar <ahmedbenhajar@hotmail.com>
2023-06-06 22:12:23 +0200

    Show badges in personal page

--- packages/client/src/app/personal_page/views/PersonalPageView.vue
+++ packages/client/src/app/personal_page/views/PersonalPageView.vue
@@ -1,17 +1,16 @@
 <script setup>
 import { Tab, Tabs } from 'vue3-tabs-component';
 import { storeToRefs } from 'pinia';
-import { useChallengeStore } from '../../challenge/stores/challenge.store.js';
-import { onMounted } from 'vue';
+import { inject, onMounted } from 'vue';
 import { useAuthStore } from '../../auth/index.js';
 import { useBadgeStore } from '../stores/personal_page.store.js';
 
-const { loading, badges } = storeToRefs(useBadgeStore());
-const { getBadges } = useBadgeStore();
+const { loading, earnedBadges } = storeToRefs(useBadgeStore());
+const { getEarnedBadges } = useBadgeStore();
 
 onMounted(async () => {
   const { user } = storeToRefs(useAuthStore());
-  await getBadges(user.value.id);
+  await getEarnedBadges(user.value.id);
 });
 </script>
 
@@ -31,7 +30,25 @@ onMounted(async () => {
           nav-item-link-active-class="active"
           panels-wrapper-class="my-4"
         >
-          <Tab name="Badges"> </Tab>
+          <Tab name="Badges">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-2">
+              <div
+                class="col-xl-3 align-items-center d-flex flex-column justify-content-center"
+                v-for="(earnedBadge, key) in earnedBadges"
+                :key="key"
+              >
+                <img
+                  class="mb-4"
+                  :src="inject('serverBaseUrl') + earnedBadge.badge.image_path"
+                  :alt="earnedBadge.badge.name"
+                />
+                <div class="fw-bold text-center">
+                  <p class="h3 text-primary">{{ earnedBadge.badge.name }}</p>
+                  <p>{{ earnedBadge.date }}</p>
+                </div>
+              </div>
+            </div>
+          </Tab>
           <Tab name="Groepen"> </Tab>
         </Tabs>
       </div>
