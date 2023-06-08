c95b8cc95323b1f17715219ff3ba63cdd570a1bd
Ahmed Benhajar <ahmedbenhajar@hotmail.com>
2023-06-08 20:46:29 +0200

    Empty state for badgelist

--- packages/client/src/app/personal_page/components/badges/BadgeList.vue
+++ packages/client/src/app/personal_page/components/badges/BadgeList.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-2">
-    <div
-      class="col-xl-3 align-items-center d-flex flex-column justify-content-center"
-      v-for="(earnedBadge, key) in props.earnedBadges"
-      :key="key"
-    >
-      <img
-        class="mb-4"
-        :src="inject('serverBaseUrl') + earnedBadge.badge.image_path"
-        :alt="earnedBadge.badge.name"
-      />
-      <div class="fw-bold text-center">
-        <p class="h3 text-primary">{{ earnedBadge.badge.name }}</p>
-        <p>{{ earnedBadge.date }}</p>
+    <template v-if="earnedBadges && earnedBadges.length">
+      <div
+        class="col-xl-3 align-items-center d-flex flex-column justify-content-center"
+        v-for="(earnedBadge, key) in props.earnedBadges"
+        :key="key"
+      >
+        <img
+          class="mb-4"
+          :src="inject('serverBaseUrl') + earnedBadge.badge.image_path"
+          :alt="earnedBadge.badge.name"
+        />
+        <div class="fw-bold text-center">
+          <p class="h3 text-primary">{{ earnedBadge.badge.name }}</p>
+          <p>{{ earnedBadge.date }}</p>
+        </div>
       </div>
-    </div>
+    </template>
+    <template v-else>
+      <small class="m-0">Je hebt nog geen badges behaald</small>
+    </template>
   </div>
 </template>
 
