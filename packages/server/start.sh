b37c8243f858f03c352de8ef016af7ff7c379fbf
Oscar Wellner <oscar.wellner@gmail.com>
2023-06-13 19:54:24 +0200

    Fix writing database config in docker container when no config is found

--- packages/server/start.sh
+++ packages/server/start.sh
@@ -1,6 +1,6 @@
 # Create the database config for a sqlite database file
 if ! [ -e ./database.json ]; then
-    cat '{ "production": { "dialect": "sqlite", "storage": "data.db" } }' >  ./database.json
+    echo '{ "production": { "dialect": "sqlite", "storage": "data.db" } }' > /app/database.json
 fi
 
 # Run database migrations to make sure all the required tables and data are present in the database
